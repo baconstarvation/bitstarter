2bd2fbceebca278626616b910b0dd22cce1b6345
Anthony Yu <ant.ucd@gmail.com>
2013-07-11 06:31:42 +0000

    Updated web.js -- link to index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var index = fs.readFileSync('index.html', 'utf-8');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
